340c34c7636287a6ba01d62bd75965635759fe99
jcjnyc <james.jackson@progyny.com>
2015-10-13 15:52:42 -0400

    updated init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-yum -y install git-all;
-git clone https://github.com/jcjnyc/bootme.git
+sudo yum -y install git-all;
+git clone https://github.com/jcjnyc/bootme.git;
 cd bootme;
 git pull;
-sh  install.sh;
+sudo sh  install.sh;
 
