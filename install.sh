db4145ca4d4dc759c7e635891a0062391032d73f
james jackson <jamescaseyjackson@gmail.com>
2015-10-24 05:57:56 -0400

    yum_list.txt

--- install.sh
+++ install.sh
@@ -1,59 +1,10 @@
 yum -y update
 
-YUMLIST="
-cronie
-emacs
-gcc
-gcc-c++
-git-all
-gpg
-gpgme
-gpgme-devel
-imake
-jq
-libgpg-error
-lynx
-mailx
-mod24_ssl
-mysql55
-mysql55-common
-mysql55-devel
-mysql55-libs
-mysql55-server
-nagios
-nagios-plugins-all
-nano
-nmap
-openssl
-perl-CPANPLUS
-php-pear.noarch
-php56.x86_64
-php56-cli.x86_64
-php56-common.x86_64
-php56-devel.x86_64
-php56-gd.x86_64
-php56-intl.x86_64
-php56-mbstring.x86_64
-php56-mcrypt.x86_64
-php56-mysqlnd.x86_64
-php56-pdo.x86_64
-php56-pecl-igbinary.x86_64
-php56-pecl-imagick.x86_64
-php56-pecl-jsonc.x86_64
-php56-pecl-jsonc-devel.x86_64
-php56-pecl-memcache.x86_64
-php56-pecl-redis.x86_64
-php56-pgsql.x86_64
-php56-process.x86_64
-php56-soap.x86_64
-php56-xml.x86_64
-php56-xmlrpc.x86_64
-yum-cron-security
-"
-
-INSTALL=`echo $YUMLIST | sed 's/\n/ /'`
-yum install -y $INSTALL
-
+## PACKAGE LIST
+for i in `cat yum_list.txt`; do
+    yum -y install ${i}
+done
+       
 ## INSTALL COMPOSER
 if [ ! -e /usr/bin/composer ]; then
     curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
@@ -69,6 +20,7 @@ if [ -d /tmp/ossec-hids ]; then
     rm -rf /tmp/ossec-hids
 fi
 git clone https://github.com/ossec/ossec-hids.git /usr/local/src/ossec-hids
+cd /usr/local/src/ossec-hids
 git checkout v2.8.3
 
 
